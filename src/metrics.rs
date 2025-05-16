291625f2a08da34d333d809ea0fa5ad6c235f8e7
Abhinav Shukla <67401627+maxprogrammer007@users.noreply.github.com>
2025-05-16 11:34:35 +0530

    Update metrics.rs

--- src/metrics.rs
+++ src/metrics.rs
@@ -5,12 +5,14 @@ use serde::{Serialize, Deserialize};
 
 /// Distance metric options for ANN search.
 #[pyclass]
-#[derive(Clone, Copy, Serialize, Deserialize)]
+#[derive(Clone, Copy, Serialize, Deserialize, Debug)]
 pub enum Distance {
     /// Euclidean (L2) distance
     Euclidean,
     /// Cosine distance (1 - cosine similarity)
     Cosine,
+    /// Manhattan (L1) distance
+    Manhattan,
 }
 
 #[pymethods]
@@ -23,11 +25,16 @@ impl Distance {
     #[classattr]
     pub const COSINE: Distance = Distance::Cosine;
 
+    /// Class attribute for Manhattan distance
+    #[classattr]
+    pub const MANHATTAN: Distance = Distance::Manhattan;
+
     /// String representation
     fn __repr__(&self) -> &'static str {
         match self {
-            Distance::Euclidean => "Distance.Euclidean",
-            Distance::Cosine    => "Distance.Cosine",
+            Distance::Euclidean => "Distance.EUCLIDEAN",
+            Distance::Cosine    => "Distance.COSINE",
+            Distance::Manhattan => "Distance.MANHATTAN",
         }
     }
 }
