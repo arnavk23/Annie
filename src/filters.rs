efee3df30483e4cc129f9730ebba88d33026eda5
Arnav Kapoor <kapoorarnav43@gmail.com>
2025-06-27 18:33:21 +0530

    Update filters.rs

--- src/filters.rs
+++ src/filters.rs
@@ -1,13 +1,19 @@
-// src/filters.rs
-
 use pyo3::prelude::*;
+use pyo3::PyObject;
 
-#[pyfunction]
-pub fn search_filter(query: &str) -> Vec<String> {
-    panic!("search_filter is not yet implemented. This function must be implemented before use.");
+/// Wrapper around a Python callback filter function
+pub struct PythonFilter {
+    callback: PyObject,
 }
 
-#[pyfunction]
-pub fn search_possible_filter() -> Vec<String> {
-    panic!("search_possible_filter is not yet implemented. This function must be implemented before use.");
+impl PythonFilter {
+    pub fn new(callback: PyObject) -> Self {
+        PythonFilter { callback }
+    }
+
+    /// Check if the given ID passes the Python filter
+    pub fn accepts(&self, py: Python<'_>, id: i64) -> PyResult<bool> {
+        let result = self.callback.call1(py, (id,))?;
+        result.extract::<bool>(py)
+    }
 }
