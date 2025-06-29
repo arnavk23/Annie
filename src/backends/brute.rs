21ced0f0bd92c12d3bc0ad7fcc85c22ab1f1e395
Gaurav Karakoti <karakotigaurav12@gmail.com>
2025-06-29 13:42:18 +0530

    Fix

--- src/backends/brute.rs
+++ src/backends/brute.rs
@@ -1,5 +1,3 @@
-// src/backends/brute.rs
-
 use crate::distance::{Distance, euclidean, cosine, manhattan, chebyshev};
 use super::ann_backend::AnnBackend;
 
@@ -31,6 +29,7 @@ impl AnnBackend for BruteForceIndex {
         }).collect();
 
         // SAFE: Use total_cmp for NaN-resistant sorting
+        scored.retain(|(_, d)| !d.is_nan());
         scored.sort_by(|a, b| a.1.total_cmp(&b.1));
         scored.truncate(k);
         scored
